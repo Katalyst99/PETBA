49784bf652167827a0e7295955c0bff967d909f7
Katalyst99 <katleho333@gmail.com>
2024-12-18 00:47:30 -0800

    Database Config

---
+++ petba_back/db.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""DB module"""
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class Database:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///petba.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "your_secret_key_here"
