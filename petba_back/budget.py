3d73616c374b35ca614b30120bc9bbd809c5e6a5
Katalyst99 <katleho333@gmail.com>
2024-12-18 01:33:32 -0800

    Added the budget model

---
+++ petba_back/budget.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""The module for Budget model"""
+from db import db
+from datetime import datetime
+
+
+class Budget(db.Model):
+    """A model named Budget for a database table named budgets"""
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    month = db.Column(db.String(10), nullable=False)
+    limit_amount = db.Column(db.Float, nullable=False)
