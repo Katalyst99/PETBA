cfe088452fc5aa2a3b3f655f2a98f7d4a8617181
Katalyst99 <katleho333@gmail.com>
2024-12-21 02:28:18 -0800

    Made necessary changes to app.py file to allow for testing of budget

--- petba_back/app.py
+++ petba_back/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from db import Database, db
 from routes.auth_routes import auth_bp
 from routes.trans_routes import transaction_bp
-# from routes.budget_routes import budget_bp
+from routes.budget_routes import budget_bp
 
 app = Flask(__name__)
 app.config.from_object(Database)
@@ -15,7 +15,7 @@ db.init_app(app)
 
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(transaction_bp, url_prefix="/transactions")
-# app.register_blueprint(budget_bp, url_prefix="/budgets")
+app.register_blueprint(budget_bp, url_prefix="/budgets")
 
 with app.app_context():
     db.create_all()
