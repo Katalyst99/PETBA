d73a541d0ece24d887349b6872ce9141f6144040
Katalyst99 <katleho333@gmail.com>
2024-12-22 03:57:57 -0800

    Updated app file to include summary routes

--- petba_back/app.py
+++ petba_back/app.py
@@ -5,6 +5,7 @@ from db import Database, db
 from routes.auth_routes import auth_bp
 from routes.trans_routes import transaction_bp
 from routes.budget_routes import budget_bp
+from routes.summary_routes import summary_bp
 
 app = Flask(__name__)
 app.config.from_object(Database)
@@ -16,6 +17,7 @@ db.init_app(app)
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(transaction_bp, url_prefix="/transactions")
 app.register_blueprint(budget_bp, url_prefix="/budgets")
+app.register_blueprint(summary_bp, url_prefix="/summary")
 
 with app.app_context():
     db.create_all()
