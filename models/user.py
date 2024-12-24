5aeab8df45e953ced189127ea4169f06049bd0a5
Katalyst99 <katleho333@gmail.com>
2024-12-24 06:32:09 -0800

    Added method that correctly stores hashed password

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """The module for User model"""
 from db import db
 from datetime import datetime
+from flask_bcrypt import generate_password_hash
 
 
 class User(db.Model):
@@ -9,3 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(250), nullable=False)
     password = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, raw_password):
+        """Method to correctly store hashed password"""
+        self.password = generate_password_hash(raw_password).decode('utf-8')
