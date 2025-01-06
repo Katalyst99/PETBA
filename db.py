b8135ff2b172bb6eba549d9097fe427d52f97642
Katalyst99 <katleho333@gmail.com>
2025-01-06 03:29:13 -0800

    Updated mysql

--- db.py
+++ db.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 """DB module"""
-from flask import Flask
+from os import getenv
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 
-
+load_dotenv()
 db = SQLAlchemy()
 
 
 class Database:
     """The Db class"""
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://petba_user:Katalyst@99@localhost/petba"
+    SQLALCHEMY_DATABASE_URI = getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "your_secret_key_here"
+    SECRET_KEY = getenv("SECRET_KEY")
+
+
+try:
+    engine = create_engine(Database.SQLALCHEMY_DATABASE_URI)
+    engine.connect()
+    print("Database connection successful.")
+    engine.close()
+except Exception as e:
+    print(f'Database connection failed: {e}')
+
+
+print(f"Loaded URI: {getenv('SQLALCHEMY_DATABASE_URI')}")
+print(f"Loaded Secret Key: {getenv('SECRET_KEY')}")
