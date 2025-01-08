84b1f876b2d015db9e14cd9790c16fdb1006a7b0
Katleho Lerato Lekale <122778859+Katalyst99@users.noreply.github.com>
2025-01-08 12:13:08 +0000

    Updated tests

--- tests/test_budget.py
+++ tests/test_budget.py
@@ -4,6 +4,7 @@ import unittest
 from app import app, db
 from flask_jwt_extended import create_access_token
 from models.budget import Budget
+from models.user import User
 
 
 class TestBudgetRoutes(unittest.TestCase):
@@ -11,13 +12,27 @@ class TestBudgetRoutes(unittest.TestCase):
     def setUp(self):
         """Set up test variables."""
         app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://petba_user:Katalyst@99@localhost/test_db'
+        app.config['SQLALCHEMY_DATABASE_URI'] = (
+            'mysql+mysqlconnector://petba_user:Katalyst@99@localhost/test_db'
+        )
         app.config['JWT_SECRET_KEY'] = 'Union'
         self.app = app.test_client()
         self.app_context = app.app_context()
         self.app_context.push()
         with app.app_context():
             db.create_all()
+            tUser = User(
+                id=1,
+                email="test@example.com"
+            )
+            tUser.set_password("testpassword123")
+            db.session.add(tUser)
+            try:
+                db.session.commit()
+                print("Test user created successfully")
+            except Exception as err:
+                print(f'Error creating test user: {str(err)}')
+                db.session.rollback()
 
     def tearDown(self):
         """Tear down test database."""
@@ -28,11 +43,13 @@ class TestBudgetRoutes(unittest.TestCase):
     def testBudgetSet(self):
         """Testing budget is set correctly"""
         with app.app_context():
-            token = create_access_token(identity=1)
+            token = create_access_token(identity='1')
+
         resp = self.app.post('/budgets/set', json={
             "month": "December",
             "limit_amount": 500
         }, headers={"Authorization": f'Bearer {token}'})
+
         self.assertEqual(resp.status_code, 201)
         self.assertIn("Budget for December set successfully",
-                      resp.get_json().get("message", ""))
+                      resp.get_json().get("msg", ""))
